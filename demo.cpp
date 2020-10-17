1508f7dc0d79b0da1bd9a91dae7d52a5007d61f5
laowang <baibinyibby@163.com>
2020-10-17 20:14:05 +0800

    commit 2

---
+++ demo.cpp
@@ -0,0 +1,7 @@
+#include"demo.h"
+
+using namespace std;
+int main(){
+    cout<<"hello world\n";
+    return 0;
+}
---
+++ demo.h
@@ -0,0 +1,5 @@
+#include<iostream>
+int func(){
+    std::cout<<1<<std::endl;
+    return 0;
+}
